19dd7d9091858030b8defe616f7dd4f890440d66
jggowthaman <jgowthaman56@gmail.com>
2026-07-24 21:13:10 +0530

    Fix frontend build CSS imports

--- gym-website/src/main.jsx
+++ gym-website/src/main.jsx
@@ -5,12 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import "./styles/Theme.css";
-import "./styles/Button.css";
-import "./styles/Common.css";
 import "./index.css";
 
-import App from "./App";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
