b86ec8a791f0c8966b5f08dc46dec7a4761c9974
jggowthaman <jgowthaman56@gmail.com>
2026-07-28 10:54:58 +0530

    nav update

--- gym-website/src/main.jsx
+++ gym-website/src/main.jsx
@@ -10,14 +10,14 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  // <StrictMode>
+  //   <BrowserRouter>
+  //     <App />
+  //   </BrowserRouter>
+  // </StrictMode>
+ <StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
-//  <StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
-//   </StrictMode>
 );
